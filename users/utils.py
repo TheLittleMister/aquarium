5d05c08423878a4af5818d66e0603e38e1ebefb0
Franklin Julian Gonzalez Perez <franklinyulian@outlook.com>
2021-06-05 00:37:28 +0000

    Fixes

--- users/utils.py
+++ users/utils.py
@@ -6,7 +6,7 @@ from courses.models import *
 
 def get_schedule(courses):
 
-    print("Let's Fix it!")
+    # print("Let's Fix it!")
 
     schedule = []
     for course in courses:
@@ -20,8 +20,8 @@ def get_schedule(courses):
                 row_course[2 + weekday] += list(course.students.all().values(
                     'id', 'identity_document', 'first_name', 'last_name', 'phone_1', 'phone_2'))
 
-                row_course[2 + weekday] = [dict(s) for s in set(frozenset(d.items())
-                                                                for d in row_course[2 + weekday])]
+                # row_course[2 + weekday] = [dict(s) for s in set(frozenset(d.items())
+                #                                                 for d in row_course[2 + weekday])]
 
                 # What if row_course[2 + weekday] already had a list? then += list() and then SET!
                 check = True
@@ -38,7 +38,7 @@ def get_schedule(courses):
             week[weekday] = list(course.students.all().values(
                 'id', 'identity_document', 'first_name', 'last_name', 'phone_1', 'phone_2'))
 
-            print(week[weekday])
+            # print(week[weekday])
 
             for i in range(7):  # 7 - Sunday
                 the_course.append(week[i])  # Week days
@@ -46,7 +46,7 @@ def get_schedule(courses):
             if the_course not in schedule:
                 schedule.append(the_course)
 
-        print(schedule)
+        # print(schedule)
     #schedule.sort(key=lambda course: course[0])
 
     return schedule
