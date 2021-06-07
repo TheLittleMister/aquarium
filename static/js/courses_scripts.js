4e82c2ba2c9df5edd67200209c9d5fb254852a18
Franklin Julian Gonzalez Perez <franklinyulian@outlook.com>
2021-06-07 16:55:14 +0000

    Scrolling

--- static/js/courses_scripts.js
+++ static/js/courses_scripts.js
@@ -1,5 +1,5 @@
 // var mysite = "http://127.0.0.1:8000";
-var mysite = "https://aquariumschool.co";
+// var mysite = "https://aquariumschool.co";
 
 // ---------THIS FUNCTION DELAYS OUR AJAX DABATASE QUERIES----------
 function delay(fn, ms) {
