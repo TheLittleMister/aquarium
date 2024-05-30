3c7d9c9a8cdfb72c4711cf863c804e09ede1162b
franklin <franklinyulian@outlook.com>
2024-05-30 11:48:14 -0500

    localStorage.removeItem('tokens');

--- front-end/src/context/AuthContext.jsx
+++ front-end/src/context/AuthContext.jsx
@@ -25,13 +25,13 @@ const AuthProvider = (props) => {
         const data = await result.json();
 
         if (!result.ok) {
-          const refreshed = await refreshTokens(
-            result.statusText,
-            tokens.refresh
-          );
-          if (refreshed) getUser(getTokens());
-          else setReady(true);
-
+          // const refreshed = await refreshTokens(
+          //   result.statusText,
+          //   tokens.refresh
+          // );
+          // if (refreshed) getUser(getTokens());
+          // else setReady(true);
+          localStorage.removeItem("tokens");
           return;
         } else {
           setUser(data);
