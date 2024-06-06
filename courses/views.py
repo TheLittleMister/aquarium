dc6f3f9ef830a46d2d39392b6b3710de5c98ce21
franklin <franklinyulian@outlook.com>
2024-06-06 14:19:48 -0500

    Statistics added and User's courses multiple selection fixed

--- courses/views.py
+++ courses/views.py
@@ -408,7 +408,7 @@ def statistics(request):
             "count"
         ).order_by("-date")[:lastN]
     
-    response["dates"] = list(data.values_list("date", flat=True))
-    response["students_count"] = list(data.values_list("count", flat=True))
+    response["dates"] = list(data.values_list("date", flat=True))[::-1]
+    response["students_count"] = list(data.values_list("count", flat=True))[::-1]
 
     return Response(response)
